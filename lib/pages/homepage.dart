16298e9b04cb7186cebc04692db95eb78454384d
renuvkelkar <renuvkelkar@gmail.com>
2021-05-15 00:34:40 +0100

    adding adress

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:geocoder/geocoder.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+import 'package:geocoder/model.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -10,21 +13,61 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   var locationMessage = '';
+  var locationMessage1 = '';
+  var CheckOuttimeMessage = '';
+  var checkinAddress = '';
+  var checkOutAddress = '';
+
+  var CheckintimeMessage = '';
   String latitude;
   String longitude;
+  Position _position;
+  Address _address;
 
   void getCurrentLocation() async {
+    Future<Address>convertCoordinatesToAddress(Coordinates coordinates)async {
+      var addresses = await Geocoder.local.findAddressesFromCoordinates(
+          coordinates);
+      return addresses.first;
+    }
+    String now = DateFormat("dd-MM-yyyy hh:mm:ss").format(DateTime.now());
     var position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
     var lat = position.latitude;
     var long = position.longitude;
+    var checkinDate = DateFormat("dd/MM/yyyy",).format(DateTime.now());
+    var checkinTime = DateFormat("hh:mm").format(DateTime.now());
+
+
+    // passing this to latitude and longitude strings
+    latitude = "$lat";
+    longitude = "$long";
+    final coordinates=new Coordinates(position.latitude, position.longitude);
+    convertCoordinatesToAddress(coordinates).then((value)=>_address=value);
+
+    setState(() {
+      locationMessage = "Latitude: $lat and Longitude: $long";
+      CheckintimeMessage = "checkin time: $checkinTime ";
+      checkinAddress = "Address from Coordinates1 : ${_address.addressLine}";
+    });
+  }
+  void getCurrentLocation1() async {
+    String now = DateFormat("dd-MM-yyyy hh:mm:ss").format(DateTime.now());
+    var position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    var lat = position.latitude;
+    var long = position.longitude;
+   // var checkinDate = DateFormat("dd/MM/yyyy",).format(DateTime.now());
+    var checkOutTime = DateFormat("hh:mm").format(DateTime.now());
+
 
     // passing this to latitude and longitude strings
     latitude = "$lat";
     longitude = "$long";
 
     setState(() {
-      locationMessage = "Latitude: $lat and Longitude: $long";
+      locationMessage1 = "Latitude: $lat and Longitude: $long";
+      CheckOuttimeMessage = "CheckOut time: $checkOutTime ";
     });
   }
 
@@ -68,12 +111,43 @@ class _HomePageState extends State<HomePage> {
             SizedBox(
               height: 05.0,
             ),
+            Text("Checkin time", style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold,)),
+            Text(CheckintimeMessage),
+            // Text("""Address from Coordinates""",
+            //     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,)),
+            SizedBox(height: 20),
+            Text(checkinAddress,
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,)),
 
             // button for taking the location
             MaterialButton(
               color: Colors.white,
               onPressed: () {
                 getCurrentLocation();
+              },
+              child: Text("Get User Location"),
+            ),
+
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              locationMessage1,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 05.0,
+            ),
+            Text("Checkout time", style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold,)),
+            Text(CheckOuttimeMessage),
+            MaterialButton(
+              color: Colors.white,
+              onPressed: () {
+                getCurrentLocation1();
               },
               child: Text("Get User Location"),
             ),
@@ -91,3 +165,4 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
